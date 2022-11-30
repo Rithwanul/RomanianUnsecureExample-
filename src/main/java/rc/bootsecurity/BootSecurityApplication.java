5faaefced25d6370896b406e60469fc97197b480
Rithwan <rithwanul@gmail.com>
2022-11-30 09:42:50 +0600

    Successfully running Spring Boot Project

--- src/main/java/rc/bootsecurity/BootSecurityApplication.java
+++ src/main/java/rc/bootsecurity/BootSecurityApplication.java
@@ -1,7 +1,12 @@
 package rc.bootsecurity;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+
+@SpringBootApplication
+@EnableScheduling
 public class BootSecurityApplication {
 
     public static void main(String[] args) {
